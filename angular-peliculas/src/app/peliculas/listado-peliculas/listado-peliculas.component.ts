742bb1ca9647102b18822adf53e3bae1b24dd5c1
JIB2017 <blacuttjuan07@gmail.com>
2025-01-28 02:49:56 -0300

    initial commit

---
+++ angular-peliculas/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-peliculas',
+  imports: [],
+  templateUrl: './listado-peliculas.component.html',
+  styleUrl: './listado-peliculas.component.css'
+})
+export class ListadoPeliculasComponent {
+
+}
