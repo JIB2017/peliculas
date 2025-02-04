f650b190674a01cd58ae4e89c2b190aa7f8a2949
JIB2017 <blacuttjuan07@gmail.com>
2025-02-04 02:13:02 -0300

    Added generic list component & Angular Material installed

--- angular-peliculas/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ angular-peliculas/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,9 +1,10 @@
 import { CurrencyPipe, DatePipe, NgOptimizedImage, UpperCasePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { ListadoGenericoComponent } from "../../compartidos/componentes/listado-generico/listado-generico.component";
 
 @Component({
   selector: 'app-listado-peliculas',
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgOptimizedImage],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgOptimizedImage, ListadoGenericoComponent],
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css'
 })
